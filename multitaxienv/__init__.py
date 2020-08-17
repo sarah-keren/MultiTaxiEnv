905892e99ab5f0ea35456b1f098ed2cbedc0fbc9
Kevin Huang <kevinhuang26@gmail.com>
2020-08-16 20:33:29 -0400

    Update __init__.py

--- multitaxienv/__init__.py
+++ multitaxienv/__init__.py
@@ -1,10 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 16 20:20:43 2020
 
-@author: Kevin
-"""
-
-from onetaxifuel_env import OneTaxiFuelEnv
-from multitaxi_env import MultiTaxiEnv
-from multitaxifuel_env import MultiTaxiFuelEnv
