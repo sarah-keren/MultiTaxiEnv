1be75005c563b8fec366830532b4e4343592ea5a
ofir.abu <OfirAbu1>
2020-09-17 08:38:29 +0300

    added idle mode

--- multitaxienv/multitaxifuel_env.py
+++ multitaxienv/multitaxifuel_env.py
@@ -32,19 +32,19 @@ class MultiTaxiFuelEnv(gym.Env):
     When the episode starts, the taxi starts off at a random square and the passenger is at a random location. 
     The taxi drives to the passenger's location, picks up the passenger, drives to the passenger's destination 
     (another one of the four specified locations), and then drops off the passenger. Once the passenger is dropped off, the episode ends.
-    
-    Observations: 
+
+    Observations:
     A list (taxis, fuels, pass_start, destinations, pass_locs):
         taxis: a list of coordinates of each taxi
         fuels: a list of fuels for each taxi
         pass_start: a list of startinig coordinates for each passenger (current position or last available)
         destinations: a list of destination coordiniates for each passenger
-        pass_locs: a list of locations of each passenger. -1 means delivered, 0 means not picked up, and positive number means the passenger 
-                    is in the corresponding taxi number 
-    
+        pass_locs: a list of locations of each passenger. -1 means delivered, 0 means not picked up, and positive number means the passenger
+                    is in the corresponding taxi number
+
     Passenger start: coordinates of each of these
     - -1: In a taxi
-    - 0: R(ed) 
+    - 0: R(ed)
     - 1: G(reen)
     - 2: Y(ellow)
     - 3: B(lue)
@@ -53,42 +53,43 @@ class MultiTaxiFuelEnv(gym.Env):
     - -1: delivered
     - 0: not in taxi
     - x: in taxi x (x is integer)
-    
-    Destinations: coordinates of each of these 
+
+    Destinations: coordinates of each of these
     - 0: R(ed)
     - 1: G(reen)
     - 2: Y(ellow)
     - 3: B(lue)
-        
+
     Fuel:
      - 0 to 10: start with 10
-    
+
     Actions:
     Actions are given as a list, each element referring to one taxi's action. Each taxi has 7 actions:
-    - 0: move south 
-    - 1: move north 
-    - 2: move east  
-    - 3: move west 
-    - 4: pickup passenger 
-    - 5: dropoff passenger 
-    - 6: refuel fuel tank 
-    
-    
-    Rewards: 
-    There is a reward of -1 for each action and an additional reward of +20 for delivering the passenger. 
+    - 0: move south
+    - 1: move north
+    - 2: move east
+    - 3: move west
+    - 4: pickup passenger
+    - 5: dropoff passenger
+    - 6: standby
+    - 7: refuel fuel tank
+
+
+    Rewards:
+    There is a reward of -1 for each action and an additional reward of +20 for delivering the passenger.
     There is a reward of -10 for executing actions "pickup", "dropoff", and "refuel" illegally.
-    
+
     Rendering:
     - blue: passenger
     - magenta: destination
     - yellow: empty taxi
     - green: full taxi
     - other letters (R, G, Y and B): locations for passengers and destinations
-    
+
     """
     metadata = {'render.modes': ['human', 'ansi']}
 
-    def __init__(self, num_taxis=2, num_passengers = 1, max_fuel = 8, map=MAP):
+    def __init__(self, num_taxis=2, num_passengers=1, max_fuel=8, map=MAP):
         self.desc = np.asarray(map, dtype='c')
 
         self.num_rows = num_rows = len(self.desc) - 2
@@ -99,9 +100,9 @@ class MultiTaxiFuelEnv(gym.Env):
         self.fuel_station2 = None
         self.fuel_stations = []
 
-        for i,row in enumerate(self.desc[1:-1]):
-            for j,char in enumerate(row[1:-1:2]):
-                loc = [i,j]
+        for i, row in enumerate(self.desc[1:-1]):
+            for j, char in enumerate(row[1:-1:2]):
+                loc = [i, j]
                 if char == b'X':
                     self.locs.append(loc)
                 elif char == b'F':
@@ -111,19 +112,19 @@ class MultiTaxiFuelEnv(gym.Env):
                     self.fuel_station2 = loc
                     self.fuel_stations.append(loc)
 
-        self.coordinates = [[i,j] for i in range(num_rows) for j in range(num_columns)]
+        self.coordinates = [[i, j] for i in range(num_rows) for j in range(num_columns)]
 
         self.max_fuel = max_fuel
 
         self.num_taxis = self.num_agents = num_taxis
-        self.num_passengers = num_passengers    
+        self.num_passengers = num_passengers
 
-        self.num_actions = 7
-        self.action_space = gym.spaces.MultiDiscrete([7 for _ in range(self.num_taxis)])
+        self.num_actions = 8
+        self.action_space = gym.spaces.MultiDiscrete([8 for _ in range(self.num_taxis)])
         self.lastaction = None
 
         self.seed()
-        self.state = None   
+        self.state = None
 
     def seed(self, seed=None):
         self.np_random, seed = utils.seeding.np_random(seed)
@@ -150,24 +151,24 @@ class MultiTaxiFuelEnv(gym.Env):
             row, col = taxi_loc
             fuel = fuels[taxi]
 
-            reward = multitaxifuel_rewards['step'] # default reward when there is no pickup/dropoff
+            reward = multitaxifuel_rewards['step']  # default reward when there is no pickup/dropoff
             done = False
             moved = False
-            
+
             # movement
-            if action == 0: # south
+            if action == 0:  # south
                 if row != max_row:
                     moved = True
                 row = min(row + 1, max_row)
-            elif action == 1:   # north
+            elif action == 1:  # north
                 if row != 0:
                     moved = True
                 row = max(row - 1, 0)
-            if action == 2 and self.desc[1 + row, 2 * col + 2] == b":":   # east
+            if action == 2 and self.desc[1 + row, 2 * col + 2] == b":":  # east
                 if col != max_col:
                     moved = True
                 col = min(col + 1, max_col)
-            elif action == 3 and self.desc[1 + row, 2 * col] == b":": # west
+            elif action == 3 and self.desc[1 + row, 2 * col] == b":":  # west
                 if col != 0:
                     moved = True
                 col = max(col - 1, 0)
@@ -176,12 +177,12 @@ class MultiTaxiFuelEnv(gym.Env):
             elif action == 4:  # pickup
                 successful_pickup = False
                 for i, loc in enumerate(pass_loc):
-                    if loc == 0 and taxi_loc == pass_start[i] and taxi+1 not in pass_loc:
+                    if loc == 0 and taxi_loc == pass_start[i] and taxi + 1 not in pass_loc:
                         pass_loc[i] = taxi + 1
                         successful_pickup = True
                         reward = multitaxifuel_rewards['pickup']
-                        break   # Picks up first passenger, modify this if capacity increases
-                if not successful_pickup: # passenger not at location
+                        break  # Picks up first passenger, modify this if capacity increases
+                if not successful_pickup:  # passenger not at location
                     reward = multitaxifuel_rewards['bad_pickup']
             elif action == 5:  # dropoff
                 successful_dropoff = False
@@ -190,22 +191,24 @@ class MultiTaxiFuelEnv(gym.Env):
                         pass_loc[i] = -1
                         reward = multitaxifuel_rewards['final_dropoff']
                         successful_dropoff = True
-                    elif loc == taxi + 1: # drops off passenger
+                    elif loc == taxi + 1:  # drops off passenger
                         pass_loc[i] = 0
                         pass_start[i] = taxi_loc
                         successful_dropoff = True
                         reward = multitaxifuel_rewards['intermediate_dropoff']
-                if not successful_dropoff: # not carrying a passenger
+                if not successful_dropoff:  # not carrying a passenger
                     reward = multitaxifuel_rewards['bad_dropoff']
+            # standby
+            elif action == 6:
+                pass
 
             # taxi refuel
-            elif action == 6:
+            elif action == 7:
                 if taxi_loc in self.fuel_stations:
                     fuel = self.max_fuel
                 else:
                     reward = multitaxifuel_rewards['bad_refuel']
 
-        
             # fuel consumption
             if moved:
                 if fuel == 0:
@@ -213,7 +216,7 @@ class MultiTaxiFuelEnv(gym.Env):
                 fuel = max(0, fuel - 1)
             if not moved and action < 4:
                 reward = multitaxifuel_rewards['hit_wall']
-                            
+
             taxis[taxi] = [row, col]
             fuels[taxi] = fuel
 
@@ -227,7 +230,7 @@ class MultiTaxiFuelEnv(gym.Env):
 
         return self.state, rewards, any(dones), {}
 
-    def render(self, mode='human', diag = True):
+    def render(self, mode='human', diag=True):
         # renders the state of the environment
 
         outfile = StringIO() if mode == 'ansi' else sys.stdout
@@ -236,20 +239,22 @@ class MultiTaxiFuelEnv(gym.Env):
         out = [[c.decode('utf-8') for c in line] for line in out]
         taxis, fuels, pass_start, destinations, pass_locs = self.state
 
-        colors = ['yellow','red','white','green','cyan','crimson','gray','magenta']*5
+        colors = ['yellow', 'red', 'white', 'green', 'cyan', 'crimson', 'gray', 'magenta'] * 5
         colored = [False for taxi in taxis]
 
-        def ul(x): return "_" if x == " " else x
-        for i,loc in enumerate(pass_locs):
-            if loc > 0:     # passenger in taxi
-                taxi_row, taxi_col = taxis[loc-1]
+        def ul(x):
+            return "_" if x == " " else x
+
+        for i, loc in enumerate(pass_locs):
+            if loc > 0:  # passenger in taxi
+                taxi_row, taxi_col = taxis[loc - 1]
                 out[1 + taxi_row][2 * taxi_col + 1] = utils.colorize(
-                    out[1 + taxi_row][2 * taxi_col + 1], colors[loc-1], highlight=True, bold=True)
-                colored[loc-1] = True
-            elif loc == 0:      # passenger not in taxi
+                    out[1 + taxi_row][2 * taxi_col + 1], colors[loc - 1], highlight=True, bold=True)
+                colored[loc - 1] = True
+            elif loc == 0:  # passenger not in taxi
                 pi, pj = pass_start[i]
                 if out[1 + pi][2 * pj + 1] != 'X':
-                    out[1 + pi][2 * pj + 1]  = 'P'
+                    out[1 + pi][2 * pj + 1] = 'P'
                 out[1 + pi][2 * pj + 1] = utils.colorize(out[1 + pi][2 * pj + 1], 'blue', bold=True)
 
         for i, taxi in enumerate(taxis):
@@ -268,48 +273,35 @@ class MultiTaxiFuelEnv(gym.Env):
             output = [moves[i] for i in self.lastaction]
             outfile.write("  ({})\n".format(' ,'.join(output)))
         for i, taxi in enumerate(taxis):
-            outfile.write("Taxi{}: Fuel: {}, Location: ({},{})\n".format(i+1,fuels[i],taxi[0],taxi[1]))
+            outfile.write("Taxi{}: Fuel: {}, Location: ({},{})\n".format(i + 1, fuels[i], taxi[0], taxi[1]))
         for i, loc in enumerate(pass_locs):
             start = tuple(pass_start[i])
-            end = tuple (destinations[i])
+            end = tuple(destinations[i])
             if loc < 0:
-                outfile.write("Passenger{}: Location: Arrived!, Destination: {}\n".format(i+1,end))
+                outfile.write("Passenger{}: Location: Arrived!, Destination: {}\n".format(i + 1, end))
             if loc == 0:
-                outfile.write("Passenger{}: Location: {}, Destination: {}\n".format(i+1,start,end))
+                outfile.write("Passenger{}: Location: {}, Destination: {}\n".format(i + 1, start, end))
             else:
-                outfile.write("Passenger{}: Location: Taxi{}, Destination: {}\n".format(i+1,loc,end))
+                outfile.write("Passenger{}: Location: Taxi{}, Destination: {}\n".format(i + 1, loc, end))
 
         # No need to return anything for human
         if mode != 'human':
             with closing(outfile):
                 return outfile.getvalue()
 
-
-    def partial_observations(self,state):
+    def partial_observations(self, state):
         def flatten(x):
             return [item for sub in x for item in sub]
+
         observations = []
         taxis, fuels, pass_start, dest, pass_loc = state
-        pass_info =  flatten(pass_start) + flatten(dest) + pass_loc
-    
+        pass_info = flatten(pass_start) + flatten(dest) + pass_loc
+
         for i in range(len(taxis)):
             obs = taxis[i] + [fuels[i]] + pass_info
             obs = np.reshape(obs, [1, len(obs)])
             observations.append(obs)
         return observations
 
-    def get_observation(self, state, agent_index):
-        def flatten(x):
-            return [item for sub in x for item in sub]
-
-        taxis, fuels, pass_start, dest, pass_loc = state
-        pass_info = flatten(pass_start) + flatten(dest) + pass_loc
-
-        obs = taxis[agent_index] + [fuels[agent_index]] + pass_info
-        obs = np.reshape(obs, [1, len(obs)])
-        return obs
-
     def get_num_jointactions(self):
-        return (self.num_actions)**(self.num_agents)
-
-
+        return (self.num_actions) ** (self.num_agents)
--- multitaxienv/onetaxifuel_env.py
+++ multitaxienv/onetaxifuel_env.py
@@ -5,7 +5,6 @@ Created on Sun Jun 14 13:14:21 2020
 @author: Kevin
 """
 
-
 import sys
 from contextlib import closing
 from io import StringIO
@@ -24,6 +23,7 @@ MAP = [
     "+---------+",
 ]
 
+
 class OneTaxiFuelEnv(discrete.DiscreteEnv):
     """
     The Taxi Problem
@@ -33,14 +33,14 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
     There are four designated locations in the grid world indicated by R(ed), G(reen), Y(ellow), and B(lue). When the episode starts, the taxi starts off at a random square with max fuel and the passenger is at a random location. The taxi drives to the passenger's location, picks up the passenger, drives to the passenger's destination (another one of the four specified locations), and then drops off the passenger. Each move consumes 1 fuel. Once the passenger is dropped off, the episode ends.
     Observations: 
     There are 5500 discrete states since there are 25 taxi positions, 5 possible locations of the passenger (including the case when the passenger is in the taxi), 4 destination locations, and 11 possible fuel states. 
-    
+
     Passenger locations:
     - 0: R(ed)
     - 1: G(reen)
     - 2: Y(ellow)
     - 3: B(lue)
     - 4: in taxi
-    
+
     Destinations:
     - 0: R(ed)
     - 1: G(reen)
@@ -48,27 +48,28 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
     - 3: B(lue)
     Fuel:
      - 0 to 10: start with 10
-        
+
     Actions:
     There are 6 discrete deterministic actions:
     - 0: move south
     - 1: move north
-    - 2: move east 
-    - 3: move west 
+    - 2: move east
+    - 3: move west
     - 4: pickup passenger
     - 5: dropoff passenger
-    - 6: refill fuel tank to 10
-    
-    Rewards: 
+    - 6: standby
+    - 7: refill fuel tank to 10
+
+    Rewards:
     There is a reward of -1 for each action and an additional reward of +20 for delivering the passenger. There is a reward of -10 for executing actions "pickup", "dropoff", "refill" illegally.
-    
+
     Rendering:
     - blue: passenger
     - magenta: destination
     - yellow: empty taxi
     - green: full taxi
     - other letters (R, G, Y and B): locations for passengers and destinations
-    
+
     state space is represented by:
         (taxi_row, taxi_col, passenger_location, destination, fuel)
     """
@@ -83,9 +84,9 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
         self.locs = []
         self.fuel_stations = []
 
-        for i,row in enumerate(self.desc[1:-1]):
-            for j,char in enumerate(row[1:-1:2]):
-                loc = [i,j]
+        for i, row in enumerate(self.desc[1:-1]):
+            for j, char in enumerate(row[1:-1:2]):
+                loc = [i, j]
                 if char == b'X':
                     self.locs.append(loc)
                 elif char == b'F':
@@ -95,11 +96,11 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
         locs = self.locs
 
         self.max_fuel = max_fuel
-        num_states = num_rows * num_columns * (max_fuel+1) * (len(locs)+1) * len(locs)
+        num_states = num_rows * num_columns * (max_fuel + 1) * (len(locs) + 1) * len(locs)
         max_row = num_rows - 1
         max_col = num_columns - 1
         initial_state_distrib = np.zeros(num_states)
-        num_actions = 7
+        num_actions = 8
         P = {state: {action: []
                      for action in range(num_actions)} for state in range(num_states)}
         for row in range(num_rows):
@@ -113,33 +114,34 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
                             for action in range(num_actions):
                                 # defaults
                                 new_row, new_col, new_pass_idx, new_fuel = row, col, pass_idx, fuel
-                                reward = taxifuel_rewards['step'] # default reward when there is no pickup/dropoff/refill
+                                reward = taxifuel_rewards[
+                                    'step']  # default reward when there is no pickup/dropoff/refill
                                 done = False
                                 taxi_loc = (row, col)
 
-                                moved = False # indicates if fuel is consumed
+                                moved = False  # indicates if fuel is consumed
 
-                                if action == 0: # south
+                                if action == 0:  # south
                                     new_row = min(row + 1, max_row)
                                     if row != max_row:
                                         moved = True
-                                elif action == 1: # north
+                                elif action == 1:  # north
                                     new_row = max(row - 1, 0)
                                     if row != 0:
                                         moved = True
-                                if action == 2 and self.desc[1 + row, 2 * col + 2] == b":": # east, no wall
+                                if action == 2 and self.desc[1 + row, 2 * col + 2] == b":":  # east, no wall
                                     new_col = min(col + 1, max_col)
                                     if col != max_col:
                                         moved = True
-                                elif action == 3 and self.desc[1 + row, 2 * col] == b":": # west, no wall
+                                elif action == 3 and self.desc[1 + row, 2 * col] == b":":  # west, no wall
                                     new_col = max(col - 1, 0)
-                                    if col != 0:    
+                                    if col != 0:
                                         moved = True
                                 elif action == 4:  # pickup
                                     if (pass_idx < 4 and taxi_loc == locs[pass_idx]):
                                         new_pass_idx = 4
                                         reward = taxifuel_rewards['pickup']
-                                    else: # passenger not at location
+                                    else:  # passenger not at location
                                         reward = taxifuel_rewards['bad_pickup']
                                 elif action == 5:  # dropoff
                                     if (taxi_loc == locs[dest_idx]) and pass_idx == 4:
@@ -148,12 +150,14 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
                                         reward = taxifuel_rewards['dropoff']
                                     elif (taxi_loc in locs) and pass_idx == 4:
                                         new_pass_idx = locs.index(taxi_loc)
-                                    else: # dropoff at wrong location
+                                    else:  # dropoff at wrong location
                                         reward = taxifuel_rewards['bad_dropoff']
-                                elif action == 6: # refill
+                                elif action == 6:  # standby
+                                    pass
+                                elif action == 7:  # refill
                                     if (taxi_loc == fuel_stations):
                                         new_fuel = 10
-                                    else: # not at fuel station
+                                    else:  # not at fuel station
                                         reward = taxifuel_rewards['bad_refuel']
                                 if moved:
                                     if fuel == 0:
@@ -173,22 +177,22 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
         i = taxi_row
         i *= self.num_columns
         i += taxi_col
-        i *= (len(self.locs)+1)
+        i *= (len(self.locs) + 1)
         i += pass_loc
         i *= len(self.locs)
         i += dest_idx
-        i *= (self.max_fuel+1)
+        i *= (self.max_fuel + 1)
         i += fuel
         return i
 
     def decode(self, i):
         out = []
-        out.append(i % (self.max_fuel+1))
-        i = i // (self.max_fuel+1)
+        out.append(i % (self.max_fuel + 1))
+        i = i // (self.max_fuel + 1)
         out.append(i % len(self.locs))
         i = i // len(self.locs)
-        out.append(i % (len(self.locs)+1))
-        i = i // (len(self.locs)+1)
+        out.append(i % (len(self.locs) + 1))
+        i = i // (len(self.locs) + 1)
         out.append(i % self.num_columns)
         i = i // self.num_columns
         out.append(i)
@@ -202,7 +206,9 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
         out = [[c.decode('utf-8') for c in line] for line in out]
         taxi_row, taxi_col, pass_idx, dest_idx, fuel = self.decode(self.s)
 
-        def ul(x): return "_" if x == " " else x
+        def ul(x):
+            return "_" if x == " " else x
+
         if pass_idx < 4:
             out[1 + taxi_row][2 * taxi_col + 1] = utils.colorize(
                 out[1 + taxi_row][2 * taxi_col + 1], 'yellow', highlight=True)
@@ -217,8 +223,10 @@ class OneTaxiFuelEnv(discrete.DiscreteEnv):
         outfile.write("\n".join(["".join(row) for row in out]) + "\n")
         outfile.write("Fuel: {}\n".format(fuel))
         if self.lastaction is not None:
-            outfile.write("  ({})\n".format(["South", "North", "East", "West", "Pickup", "Dropoff", "Refill"][self.lastaction]))
-        else: outfile.write("\n")
+            outfile.write(
+                "  ({})\n".format(["South", "North", "East", "West", "Pickup", "Dropoff", "Refill"][self.lastaction]))
+        else:
+            outfile.write("\n")
 
         # No need to return anything for human
         if mode != 'human':
