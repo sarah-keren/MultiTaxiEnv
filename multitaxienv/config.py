848027f26b6628101f2d647f1e831a7eec012f95
ofir.abu <OfirAbu1>
2020-09-23 08:56:46 +0300

    added seperated files for idle mode

--- multitaxienv/config.py
+++ multitaxienv/config.py
@@ -41,3 +41,34 @@ multitaxifuel_rewards_w_idle = dict(
     final_dropoff = 100,
     hit_wall = -20,
 )
+
+taxifuel_rewards = dict(
+    step = -1,
+    no_fuel = -10,
+    bad_pickup = -10,
+    bad_dropoff = -10,
+    bad_refuel = -10,
+    pickup = -1,
+    dropoff = 20,
+)
+
+multitaxi_rewards = dict(
+    step = -1,
+    bad_pickup = -10,
+    bad_dropoff = -10,
+    pickup = -1,
+    intermediate_dropoff = -1,
+    final_dropoff = 100,
+)
+
+multitaxifuel_rewards = dict(
+    step = -1,
+    no_fuel = -10,
+    bad_pickup = -10,
+    bad_dropoff = -10,
+    bad_refuel = -10,
+    pickup = -1,
+    intermediate_dropoff = -1,
+    final_dropoff = 100,
+    hit_wall = -20,
+)
