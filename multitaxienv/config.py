e7970489b295e1306f8ab9130bfe93fffbcbd110
ofir.abu <OfirAbu1>
2020-09-24 20:01:14 +0300

    tested the idle mode envs on the test file, passed

--- multitaxienv/config.py
+++ multitaxienv/config.py
@@ -1,3 +1,47 @@
+taxifuel_rewards_w_idle = dict(
+    step = -1,
+    no_fuel = -10,
+    bad_pickup = -10,
+    bad_dropoff = -10,
+    bad_refuel = -10,
+    pickup = -1,
+    dropoff = 20,
+    turn_engine_off = 0,
+    standby_engine_off = 0,
+    turn_engine_on = -1,
+    standby_engine_on = -1,
+)
+
+multitaxi_rewards_w_idle = dict(
+    step = -1,
+    bad_pickup = -10,
+    bad_dropoff = -10,
+    pickup = -1,
+    intermediate_dropoff = -1,
+    final_dropoff = 100,
+    standby_engine_off = 0,
+    turn_engine_on = -1,
+    turn_engine_off = 0,
+    standby_engine_on = -1,
+    bad_refuel = -10,
+)
+
+multitaxifuel_rewards_w_idle = dict(
+    step = -1,
+    no_fuel = -10,
+    bad_pickup = -10,
+    bad_dropoff = -10,
+    bad_refuel = -10,
+    pickup = -1,
+    standby_engine_off = 0,
+    turn_engine_on = -1,
+    turn_engine_off = 0,
+    standby_engine_on = -1,
+    intermediate_dropoff = -1,
+    final_dropoff = 100,
+    hit_wall = -20,
+)
+
 taxifuel_rewards = dict(
     step = -1,
     no_fuel = -10,
