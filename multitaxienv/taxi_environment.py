ef585d3d6722d2a48556a8d34888d62dd1cd4015
sarah-keren <sarah.e.keren@gmail.com>
2021-02-09 22:23:58 +0200

    module change in taxi_env

--- multitaxienv/taxi_environment.py
+++ multitaxienv/taxi_environment.py
@@ -3,7 +3,7 @@
 import gym
 from gym.utils import seeding
 import numpy as np
-from config import TAXI_ENVIRONMENT_REWARDS, BASE_AVAILABLE_ACTIONS, ALL_ACTIONS_NAMES
+from multitaxienv.config import TAXI_ENVIRONMENT_REWARDS, BASE_AVAILABLE_ACTIONS, ALL_ACTIONS_NAMES
 from gym.spaces import Box, Tuple, MultiDiscrete
 from utils import rendering_utils, basic_utils, actions_utils, \
     observation_utils, reward_utils
